0b51c90c44d65aa256e5beff670e9ce5300e2cf7
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-27 20:19:27 +0530

    Imitating udemy course till starting point

---
+++ src/main/java/com/yg/ClinicSpringReact/api/v1/model/CategoryDTO.java
@@ -0,0 +1,9 @@
+package com.yg.ClinicSpringReact.api.v1.model;
+
+/*
+expose this class using REST controller
+ */
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/yg/ClinicSpringReact/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.yg.ClinicSpringReact.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+/*
+Doubt1: why extends JpaRepository
+Doubt2 : why repository package created
+ */
+public interface CategoryRepository extends JpaRepository {
+}

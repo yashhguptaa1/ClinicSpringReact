8511c4386b8f28e60e0d89f0c4778884b43326c9
yashhguptaa1 <yashhguptaa1@gmail.com>
2020-05-27 19:45:14 +0530

    First Commit

---
+++ src/main/java/com/yg/ClinicSpringReact/ClinicSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.yg.ClinicSpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSpringReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yg/ClinicSpringReact/ClinicSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yg.ClinicSpringReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
